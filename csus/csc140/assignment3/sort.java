fcf7d148f0b1535689a741c43e6761445a5e468e
Gabe Nicula <gabenicula@Gabes-Air.hsd1.ca.comcast.net>
2023-03-04 20:48:01 -0800

    Quicksort done

--- csus/csc140/assignment3/sort.java
+++ csus/csc140/assignment3/sort.java
@@ -1,14 +1,16 @@
 import java.util.Random;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Sorting {
 	final int MAX_SIZE = 10000000;
 
 	// Set this to true if you wish the arrays to be printed.
-	final static boolean OUTPUT_DATA = true;
+	final static boolean OUTPUT_DATA = false;
 	
 	public static String sortAlg= null;
 	public static  int size = 0;
+	public static Random randIntGen = new Random();
 	
 	public static void main(String[] args) {
 		readInput();
@@ -96,6 +98,12 @@ public class Sorting {
 		int i;
 		for(i = 0; i < size; i++)
 			data[i] = new Random().nextInt(10000000);
+//		int data_bad[] = new int[] {2871684, 9760106, 4510918, 4660545,
+//		  276628, 6753049, 5867792, 2693707, 456969, 1802116 };
+//		for(i = 0; i < size; i++)
+//			data[i] = data_bad[i]; 
+			
+		
 	}
 	/*****************************************************************************/
 
@@ -193,12 +201,80 @@ public class Sorting {
 		//System.out.println("MergeSort");
 	}
 	/*****************************************************************************/
-
+	public static int ChoosePivot(int data[], int lo, int hi, int pivotCase)
+	{
+		int pivot = hi;
+		switch(pivotCase)
+		{
+			case 0: 
+//				pivot = hi;
+				break;
+			case 1:
+				pivot = lo + randIntGen.nextInt(hi-lo+1);
+				break;
+			case 2:
+				int first = lo + randIntGen.nextInt(hi-lo+1);
+				int second = lo + randIntGen.nextInt(hi-lo+1);
+				int third = lo + randIntGen.nextInt(hi-lo+1);
+				
+				pivot = second;
+				
+				if ((data[second] <= data[first] && data[second] >= data[third]) || 
+						data[second] >= data[first] && data[second] <= data[third])
+				{
+					pivot = second;
+				} else if ((data[first] >= data[third] && data[first] <= data[second]) ||
+						(data[first] <= data[third] && data[first] >= data[second]))
+				{
+					pivot = first;
+				} else if ((data[third] >= data[first] && data[third] <= data[second]) ||
+						(data[third] <= data[first] && data[third] >= data[second]))
+				{
+					pivot = third;
+				}
+		}
+		return pivot;
+	}
+	/*****************************************************************************/
+	
+	public static int Partition(int data[], int lo, int hi, int pivotCase)
+	{
+		//System.out.println("lo: " + lo + " hi: " + hi);
+		int i = lo - 1;
+		
+//		if(randPivot)
+//		{
+//			int randPivotIndex = lo + new Random().nextInt(hi-lo+1);
+//			swap(randPivotIndex, hi, data);
+//		}
+		
+		int pivotIndex = ChoosePivot(data, lo, hi, pivotCase);
+		swap(pivotIndex, hi, data);
+		
+		int x = data[hi];
+		//System.out.println("data[]: " + Arrays.toString(data) + "pivot: " + x);
+		for (int j = lo; j < hi; ++j)
+		{
+			if(data[j] < x)
+			{
+				swap(j, (i+1), data);
+				++i;
+			}
+		}
+		swap(hi, (i+1), data);
+		return i+1;
+	}
+	/*****************************************************************************/
+	
 	public static void QuickSort(int data[], int lo, int hi)
 	{
-		//Write your code here
-		//You may create other functions if needed 
 		//System.out.println("QuickSort");
+		if (hi - lo >= 1) 
+		{
+			int pivot = Partition(data, lo, hi, 1);
+			QuickSort(data, lo, pivot-1);
+			QuickSort(data, pivot+1, hi);
+		}
 		
 	}
 	/*****************************************************************************/
@@ -208,6 +284,7 @@ public class Sorting {
 		//Write your code here
 		//Your code should simply call the STL sorting function  
 		//System.out.println("STLSort");
+		Arrays.sort(data);
 		
 	}
 	/*****************************************************************************/
