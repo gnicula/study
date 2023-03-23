a331cd5c7f7623962888b35fd757b600e8091ccb
gnicula <72640399+gnicula@users.noreply.github.com>
2023-03-22 20:54:19 -0700

    Delete Fixed.java

--- csus/csc133/A2Prj/Fixed.java
+++
@@ -1,9 +0,0 @@
-package com.mycompany.a2;
-
-public class Fixed extends GameObject{
-	
-	public Fixed(int size, double x, double y, int color, GameWorld w) {
-		super(size, x, y, color, w);
-	}
-
-}
