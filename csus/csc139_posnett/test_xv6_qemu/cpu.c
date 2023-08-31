2e53a0850bca343de3ce1e82ca1290403b8b131c
Gabe Nicula <gabenicula@Gabes-Air.hsd1.ca.comcast.net>
2023-08-30 19:45:10 -0700

    first example in the book

--- csus/csc139_posnett/test_xv6_qemu/cpu.c
+++ csus/csc139_posnett/test_xv6_qemu/cpu.c
@@ -1,20 +1,21 @@
 #include <stdio.h>
-2 #include <stdlib.h>
-3 #include <sys/time.h>
-4 #include <assert.h>
-5 #include "common.h"
-6
-7 int
-8 main(int argc, char *argv[])
-9 {
-10 if (argc != 2) {
-11 fprintf(stderr, "usage: cpu <string>\n");
-12 exit(1);
-13 }
-14 char *str = argv[1];
-15 while (1) {
-16 Spin(1);
-17 printf("%s\n", str);
-18 }
-19 return 0;
-20 }
+#include <stdlib.h>
+#include <sys/time.h>
+#include <assert.h>
+#include "common.h"
+
+int main(int argc, char *argv[])
+{
+  if (argc != 2)
+  {
+    fprintf(stderr, "usage: cpu <string>\n");
+    exit(1);
+  }
+  char *str = argv[1];
+  while (1)
+  {
+    Spin(1);
+    printf("%s\n", str);
+  }
+  return 0;
+}
