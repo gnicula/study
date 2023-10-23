bb0976ca11b9b331fc672050112f3f0c63e270e6
Gabe Nicula <gabenicula@Gabes-Air-2.hsd1.ca.comcast.net>
2023-10-22 20:11:25 -0700

    finished requirements

--- csus/csc139_posnett/UNIX_Shell/Unix_Shell.c
+++ csus/csc139_posnett/UNIX_Shell/Unix_Shell.c
@@ -1,3 +1,5 @@
+#include <ctype.h>
+#include <fcntl.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -20,13 +22,56 @@ void error() {
 
 int add_path(char* str) {
     char* token;
+    for (int i = 0; i < num_paths; ++i) {
+        for (int j = 0; j < MAX_PATH_LENGTH; ++j) {
+            wish_paths[i][j] = 0;
+        }
+    }
     int i = 0;
-
     while ((token = strsep(&str, " "))) {
         strcpy(wish_paths[i], token);
         ++i;
     }
     return i;
+}
+
+int character_search(char* str, char c) {
+    while (*str != 0) {
+        // printf("str: %s\n", str);
+        if (*str == c) {
+            return 1;
+        }
+        ++str;
+    }
+    return 0;
+}
+
+char* trim_white_space(char *str)
+{
+    if (str == NULL) {
+        return str;
+    }
+    char* end;
+
+    // Trim leading space
+    while(isspace((unsigned char)*str)) {
+        str++;
+    }
+
+    if(*str == 0) {
+        return str;
+    }
+
+    // Trim trailing space
+    end = str + strlen(str) - 1;
+    while(end > str && isspace((unsigned char)*end)) {
+        end--;
+    }
+
+    // Write new null terminator character
+    end[1] = '\0';
+
+    return str;
 }
 
 int num_arguments(char* str) {
@@ -54,8 +99,18 @@ int num_arguments(char* str) {
     return nargs;
 }
 
-void execute_non_built_in_command(char* name, char* cmd, char* pipe) {
-    // printf("name: %s, cmd: %s", name, cmd);
+void execute_non_built_in_command(char* name, char* cmd, char* filename) {
+    int fout = -1;
+    if (filename != NULL) {
+        // command output is redirected to a file, create it
+        fout = open(filename, O_WRONLY | O_TRUNC | O_CREAT);
+        if (fout == -1) {
+            error();
+            // invalid redirection
+            return;
+        } 
+    }
+    // printf("name: %s, cmd: %s, filename: %s", name, cmd, filename);
     fflush(stdout);
     int pid = fork();
     if (pid == 0) {
@@ -63,13 +118,19 @@ void execute_non_built_in_command(char* name, char* cmd, char* pipe) {
         int nargs = num_arguments(cmd);
         // printf("in child, n_args: %d\n", nargs);
 
+        // define the argument table, execv expects
+        // a table of pointers to strings
+        // must be null-terminated (\0)
         char* argv[nargs + 2];
         argv[0] = name;
         for (int i = 1; i < nargs + 1; ++i) {
             argv[i] = strsep(&cmd, " ");
         }
         argv[nargs + 1] = NULL;
-        fflush(stdout);
+        if (fout > 0) {
+            dup2(fout, STDOUT_FILENO);
+            dup2(fout, STDERR_FILENO);
+        }
         // printf("execv name: %s, argv: %s", name, argv[0]);
         if (execv(name, argv) == -1) {
             error();
@@ -77,7 +138,7 @@ void execute_non_built_in_command(char* name, char* cmd, char* pipe) {
         exit(0);
     } else if (pid > 0) {
         // parent process
-        wait(NULL);
+        // wait(NULL);
     } else {
         // could not create process
         // printf("Could not create process");
@@ -85,8 +146,15 @@ void execute_non_built_in_command(char* name, char* cmd, char* pipe) {
     }
 }
 
-void resolve_non_built_in_command(char* name, char* cmd, char* pipe) {
+void resolve_non_built_in_command(char* name, char* cmd, char* filename) {
+    // printf("Name: |%s|, cmd: %s", name, cmd);
+    if (name == NULL || name[0] == '\0') {
+        error();
+        return;
+    }
     if ((access(name, X_OK) != 0)) {
+        // command was not found in current directory
+        // search for it in the registered paths
         int found_in_path = 0;
         for (int i = 0; i < num_paths; ++i) {
             char* full_name = strdup(wish_paths[i]);
@@ -95,7 +163,7 @@ void resolve_non_built_in_command(char* name, char* cmd, char* pipe) {
 
             if (access(full_name, X_OK) == 0) {
                 found_in_path = 1;
-                execute_non_built_in_command(full_name, cmd, pipe);
+                execute_non_built_in_command(full_name, cmd, filename);
                 break;
             }
         }
@@ -103,33 +171,41 @@ void resolve_non_built_in_command(char* name, char* cmd, char* pipe) {
             error();
         }
     } else {
-        execute_non_built_in_command(name, cmd, pipe);
+        execute_non_built_in_command(name, cmd, filename);
     }
 }
 
 void execute_single_command(char* str) {
-    char* token = strsep(&str, " ");
-    // printf("sc: %s\n", token);
+    int has_indirection = character_search(str, '>');
+    char* cmd = strsep(&str, ">");
+    char* cmd_name = strsep(&cmd, " ");
+    // printf("single command name: %s\n", cmd_name);
     int result = 0;
-    if (strcmp(token, "cd") == 0) {
-        // printf("str: %s|\n", str);
-        result = chdir(str);
+    if (strcmp(cmd_name, "cd") == 0) {
+        // printf("command args: %s|\n", cmd);
+        result = chdir(cmd);
         // printf("result: %d\n", result);
         if (result != 0) {
             error();
         }
-    } else if(strcmp(token, "path") == 0) {
-        num_paths = add_path(str);
+    } else if(strcmp(cmd_name, "path") == 0) {
+        num_paths = add_path(cmd);
         // printf("%s", wish_paths[0]);
-    } else if(strcmp(token, "exit") == 0) {
-        if (str == NULL) {
+    } else if(strcmp(cmd_name, "exit") == 0) {
+        if (cmd == NULL) {
             exit(0);
         } else {
             error();
         }
     } else {
-        // try search in path and execute with fork
-        resolve_non_built_in_command(token, str, NULL);
+        char* filename = trim_white_space(str);
+        cmd_name = trim_white_space(cmd_name);
+        if (has_indirection && (filename == NULL || character_search(filename, ' '))) {
+            error();
+        } else {
+            // try search in path and execute with fork
+            resolve_non_built_in_command(cmd_name, cmd, filename);
+        }
     }
 }
 
@@ -137,65 +213,80 @@ void execute_line_commands(char* str) {
     char* token = NULL;
     char* copy_line = strdup(str);
     while ((token = strsep(&copy_line, "&"))) {
+        token = trim_white_space(token);
         // printf("lc: %s\n", token);
-        execute_single_command(token);
+        if (strlen(token) > 0) {
+            execute_single_command(token);
+        }
+    }
+    while (wait(NULL) > 0) {
+        // wait for all children to complete
     }
 }
 
-int main(int argc, char *argv[]) {
-    
+void process_line(char* input) {
+    input[strcspn(input, "\r\n")] = 0;
+    char* trimmed_input = trim_white_space(input);
+    if (trimmed_input != NULL && strlen(trimmed_input) > 0) {
+        execute_line_commands(trimmed_input);
+    }
+}
+
+void run_interactive_mode() {
+    // Interactive mode
+    char* input = NULL;
+    size_t input_size = 0;
+
+    while (getline(&input, &input_size, stdin) == -1) {
+        printf("wish> ");
+        process_line(input);
+        free(input);
+        input = NULL;
+        input_size = 0;
+    }
+    free(input); // Free the dynamically allocated input buffer
+    input = NULL;
+    input_size = 0;
+
+}
+
+void run_batch_mode(char* filename) {
     char *input = NULL;
     size_t input_size = 0;
+
+    // Batch mode: Read commands from a file
+    FILE *batch_file = fopen(filename, "r"); 
+    if (batch_file == NULL) {
+        error();
+        exit(1);
+    }
+
+    while (getline(&input, &input_size, batch_file) != -1) {
+        process_line(input);
+        free(input);
+        input = NULL;
+        input_size = 0;
+    }
+    free(input); // Free the dynamically allocated input buffer
+    input = NULL;
+    input_size = 0;
+    fclose(batch_file);
+
+}
+
+int main(int argc, char *argv[]) {
 
     if (argc > 2) {
         error();
         exit(1);
     }
-
     num_paths = add_path("/bin");
 
     if (argc == 2) {
-        // Batch mode: Read commands from a file
-        FILE *batch_file = fopen(argv[1], "r");
-        if (batch_file == NULL) {
-            error();
-            exit(1);
-        }
-        while (getline(&input, &input_size, batch_file) != -1) {
-            // Remove \r\n from end of line
-            input[strcspn(input, "\r\n")] = 0;
-            execute_line_commands(input);
-        }
-        free(input); // Free the dynamically allocated input buffer
-        fclose(batch_file);
+        run_batch_mode(argv[1]);
     } else {
-        // Interactive mode
-        int should_exit = 0; // Indicates if the shell should exit
-
-        while (!should_exit) {
-            printf("wish> ");
-            if (getline(&input, &input_size, stdin) == -1) {
-                break; // Exit on EOF or error
-            } else {
-                // Remove \r\n from end of line
-                input[strcspn(input, "\r\n")] = 0;
-            }
-
-            // Check if the user entered "exit"
-            if (strcmp(input, "exit") == 0) {
-                should_exit = 1;
-            } else {
-                execute_line_commands(input);
-            }
-
-            // Reset the input buffer for the next iteration
-            free(input);
-            input = NULL;
-            input_size = 0;
-        }
+        run_interactive_mode();
     }
-
-    // free(input);
     return 0;
 }
 
