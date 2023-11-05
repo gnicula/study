a2b8123537c78cd1be560cbf670f8aebd1538a73
Gabe Nicula <gabenicula@Gabes-MacBook-Air-2.local>
2023-11-04 23:07:03 -0700

    worked on malloc BEST_FIT

--- csus/csc139_posnett/Linux_Memory_Allocation/umem.c
+++ csus/csc139_posnett/Linux_Memory_Allocation/umem.c
@@ -7,6 +7,9 @@
 
 #include "umem.h"
 
+size_t const MARKER_VALUE = 0x12345678;
+void *MARKER = (void*)MARKER_VALUE;
+
 typedef struct node_t
 {
   unsigned int size;
@@ -86,10 +89,10 @@ void *mallocBestFit(size_t size)
     return NULL;
   }
   size_t spaceLeft = curBestFit->size - adjustedSize;
-  if (spaceLeft > nodeSize + 8)
+  if (spaceLeft >= nodeSize + 8)
   {
-    node_t *newNode = (node_t *)((char *)curBestFit + adjustedSize);
-    newNode->size = spaceLeft;
+    node_t *newNode = (node_t *)((char *)curBestFit + adjustedSize + nodeSize);
+    newNode->size = spaceLeft - nodeSize;
     newNode->next = curBestFit->next;
     // TODO: Check if we are allowed to use 2 pointers in our node structure.
     if (curBestFit == g_head)
@@ -103,15 +106,40 @@ void *mallocBestFit(size_t size)
         if (p->next == curBestFit)
         {
           p->next = newNode;
+          break;
         }
       }
     }
   }
+  else
+  {
+    if (curBestFit == g_head)
+    {
+      g_head = NULL;
+    }
+    else
+    {
+      for (node_t *p = g_head; p != NULL; p = p->next)
+      {
+        if (p->next == curBestFit)
+        {
+          p->next = curBestFit->next;
+          break;
+        }
+      }
+    }
+  }
+  curBestFit->size = adjustedSize;
+  curBestFit->next = MARKER;
   return (void *)((char *)curBestFit + nodeSize);
 }
 
 void *umalloc(size_t size)
 {
+  if (g_head == NULL) 
+  {
+    return NULL;
+  }
   switch (g_allocationAlgo)
   {
   case BEST_FIT:
@@ -120,4 +148,42 @@ void *umalloc(size_t size)
     printf("Not implemented.");
   }
   return NULL;
+}
+
+void coalesce()
+{
+  node_t *ptr = g_head;
+  char *end = NULL;
+  while (ptr != NULL)
+  {
+    end = (char *)ptr + nodeSize + ptr->size;
+    // printf("Head: %p\n", g_head);
+    node_t *ptrEnd = (node_t *)(end);
+    node_t *cmp = ptr->next;
+    while (cmp != NULL)
+    {
+      // printf("ptr: %p, cmp: %p, ptrEnd: %p\n", ptr, cmp, ptrEnd);
+      if (cmp == ptrEnd)
+      {
+        // printf("Entered Coalesce if\n");
+        ptr->size = ptr->size + ptrEnd->size + nodeSize;
+        ptr->next = ptrEnd->next;
+      }
+      cmp = cmp->next;
+    }
+    ptr = ptr->next;
+  }
+}
+
+int ufree(void *ptr)
+{
+  node_t *nodePtr = (node_t *)((char *)ptr - nodeSize);
+  if (nodePtr->next != MARKER)
+  {
+    return -1;
+  }
+  nodePtr->next = g_head;
+  g_head = nodePtr;
+  coalesce();
+  return 0;
 }
--- csus/csc139_posnett/Linux_Memory_Allocation/umem_test.c
+++ csus/csc139_posnett/Linux_Memory_Allocation/umem_test.c
@@ -20,10 +20,20 @@ int test_umemdump()
   return 0;
 }
 
-int test_umalloc(size_t size)
+void* test_umalloc(size_t size)
 {
   void* ptr = umalloc(size);
   if (ptr != NULL) 
+  {
+    printf("Passed\n");
+  }
+  return ptr;
+}
+
+int test_ufree(void *ptr)
+{
+  int result = ufree(ptr);
+  if (result == 0)
   {
     printf("Passed\n");
   }
@@ -34,7 +44,27 @@ int main()
 {
   test_umeminit(128);
   test_umemdump();
-  test_umalloc(75);
+  // Scenario 1: Allocate block and immediately deallocate it.
+  // Look for coalesce to return the initial memory.
+  void *result1 = test_umalloc(75);
+  test_umemdump();
+  test_ufree(result1);
+  test_umemdump();
+  
+  // Scenario 2: Allocate 3 blocks and deallocate 2 of them.
+  // Observe memory fragmentation with no coalescing.
+  void *result2 = test_umalloc(15);
+  test_umemdump();
+  void *result3 = test_umalloc(16);
+  test_umemdump();
+  void *result4 = test_umalloc(17);
+  test_umemdump();
+  test_ufree(result3);
+  test_umemdump();
+  // BUG: Does not coalesce correctly.
+  test_ufree(result4);
+  test_umemdump();
+  test_ufree(result2);
   test_umemdump();
 
   return 0;
