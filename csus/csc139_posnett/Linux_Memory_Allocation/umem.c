076647d982f4eabde168fa70d912f4f2af1eecaa
Gabe Nicula <gabenicula@Gabes-MacBook-Air-2.local>
2023-11-04 20:50:10 -0700

    added malloc, memdump, and meminit

--- csus/csc139_posnett/Linux_Memory_Allocation/umem.c
+++ csus/csc139_posnett/Linux_Memory_Allocation/umem.c
@@ -7,22 +7,117 @@
 
 #include "umem.h"
 
+typedef struct node_t
+{
+  unsigned int size;
+  struct node_t *next;
+} node_t;
+
+int g_allocationAlgo = 0;
+
+node_t *g_head = (node_t *)NULL;
+
+size_t roundup(size_t const sz)
+{
+  if (sz % 8 == 0)
+  {
+    return sz;
+  }
+  return sz + (8 - sz % 8);
+}
+
+size_t const nodeSize = sizeof(node_t);
+
 int umeminit(size_t sizeOfRegion, int allocationAlgo)
 {
+  g_allocationAlgo = allocationAlgo;
+  // FIX FOR UBUNTU 22
   // open the /dev/zero device
-  int fd = open("/dev/zero", O_RDWR);
+  // int fd = open("/dev/zero", O_RDWR);
 
   // sizeOfRegion (in bytes) needs to be evenly divisible by the page size
-  void *ptr = mmap(NULL, sizeOfRegion, PROT_READ | PROT_WRITE, MAP_PRIVATE, fd, 0);
+  void *ptr = mmap(NULL, sizeOfRegion, PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANON, -1, 0);
   if (ptr == MAP_FAILED)
   {
     perror("mmap");
     exit(1);
   }
-
+  printf("Node Size: %zu\n", nodeSize);
+  g_head = (node_t *)ptr;
+  g_head->next = NULL;
+  g_head->size = sizeOfRegion - nodeSize;
   // close the device (don't worry, mapping should be unaffected)
-  close(fd);
+  // close(fd);
   return 0;
 }
 
+void umemdump()
+{
+  node_t *node = g_head;
+  while (node != NULL)
+  {
+    printf("%p: %u\n", node, node->size);
+    node = node->next;
+  }
+}
 
+void *mallocBestFit(size_t size)
+{
+  node_t *curBestFit = NULL;
+  node_t *ptr = g_head;
+  int adjustedSize = roundup(size);
+  while (ptr != NULL)
+  {
+    if (ptr->size >= adjustedSize)
+    {
+      if (curBestFit == NULL)
+      {
+        curBestFit = ptr;
+      }
+      else if (curBestFit->size > ptr->size)
+      {
+        curBestFit = ptr;
+      }
+    }
+    ptr = ptr->next;
+  }
+  if (curBestFit == NULL)
+  {
+    return NULL;
+  }
+  size_t spaceLeft = curBestFit->size - adjustedSize;
+  if (spaceLeft > nodeSize + 8)
+  {
+    node_t *newNode = (node_t *)((char *)curBestFit + adjustedSize);
+    newNode->size = spaceLeft;
+    newNode->next = curBestFit->next;
+    // TODO: Check if we are allowed to use 2 pointers in our node structure.
+    if (curBestFit == g_head)
+    {
+      g_head = newNode;
+    }
+    else
+    {
+      for (node_t *p = g_head; p != NULL; p = p->next)
+      {
+        if (p->next == curBestFit)
+        {
+          p->next = newNode;
+        }
+      }
+    }
+  }
+  return (void *)((char *)curBestFit + nodeSize);
+}
+
+void *umalloc(size_t size)
+{
+  switch (g_allocationAlgo)
+  {
+  case BEST_FIT:
+    return mallocBestFit(size);
+  default:
+    printf("Not implemented.");
+  }
+  return NULL;
+}
--- csus/csc139_posnett/Linux_Memory_Allocation/umem_test.c
+++ csus/csc139_posnett/Linux_Memory_Allocation/umem_test.c
@@ -3,16 +3,39 @@
 
 #include "umem.h"
 
-int test_umeminit() {
-  int result = umeminit(100, BEST_FIT);
-  if (result == 0) {
-    printf("Passed\n");    
+int test_umeminit(size_t size)
+{
+  int result = umeminit(size, BEST_FIT);
+  if (result == 0)
+  {
+    printf("Passed\n");
   }
   return 0;
 }
 
-int main() {
-  test_umeminit();
-  
+int test_umemdump()
+{
+  umemdump();
+
+  return 0;
+}
+
+int test_umalloc(size_t size)
+{
+  void* ptr = umalloc(size);
+  if (ptr != NULL) 
+  {
+    printf("Passed\n");
+  }
+  return 0;
+}
+
+int main()
+{
+  test_umeminit(128);
+  test_umemdump();
+  test_umalloc(75);
+  test_umemdump();
+
   return 0;
 }
