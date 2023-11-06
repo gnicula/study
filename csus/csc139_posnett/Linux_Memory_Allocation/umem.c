d8619a3d1c9d5a8191b2539e7af830afebb00473
Gabe Nicula <gabenicula@Gabes-Air-2.hsd1.ca.comcast.net>
2023-11-05 20:22:20 -0800

    added all other algorithms

--- csus/csc139_posnett/Linux_Memory_Allocation/umem.c
+++ csus/csc139_posnett/Linux_Memory_Allocation/umem.c
@@ -8,17 +8,18 @@
 #include "umem.h"
 
 size_t const MARKER_VALUE = 0x12345678;
-void *MARKER = (void*)MARKER_VALUE;
+void *MARKER = (void *)MARKER_VALUE;
 
-typedef struct node_t
+typedef struct node_t_str
 {
   unsigned int size;
-  struct node_t *next;
+  struct node_t_str *next;
 } node_t;
 
 int g_allocationAlgo = 0;
 
 node_t *g_head = (node_t *)NULL;
+node_t *g_nextFitStart = (node_t *)NULL;
 
 size_t roundup(size_t const sz)
 {
@@ -39,16 +40,20 @@ int umeminit(size_t sizeOfRegion, int allocationAlgo)
   // int fd = open("/dev/zero", O_RDWR);
 
   // sizeOfRegion (in bytes) needs to be evenly divisible by the page size
-  void *ptr = mmap(NULL, sizeOfRegion, PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANON, -1, 0);
+  int myPageSize = getpagesize();
+  size_t pageRemainder = (sizeOfRegion + nodeSize) % myPageSize;
+  size_t newSizeOfRegion = ((sizeOfRegion + nodeSize) / myPageSize) * myPageSize + (pageRemainder > 0 ? myPageSize : 0);
+  void *ptr = mmap(NULL, newSizeOfRegion, PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANON, -1, 0);
   if (ptr == MAP_FAILED)
   {
     perror("mmap");
     exit(1);
   }
-  printf("Node Size: %zu\n", nodeSize);
+  // printf("Node Size: %zu\n", nodeSize);
   g_head = (node_t *)ptr;
   g_head->next = NULL;
-  g_head->size = sizeOfRegion - nodeSize;
+  g_head->size = sizeOfRegion;
+  g_nextFitStart = g_head;
   // close the device (don't worry, mapping should be unaffected)
   // close(fd);
   return 0;
@@ -64,6 +69,58 @@ void umemdump()
   }
 }
 
+void skipNode(node_t *node, node_t *newNode)
+{
+  for (node_t *p = g_head; p != NULL; p = p->next)
+  {
+    if (p->next == node)
+    {
+      p->next = newNode;
+      break;
+    }
+  }
+}
+
+void umallocSpaceSaver(node_t *allocated, size_t requestedSpace)
+{
+  size_t spaceLeft = allocated->size - requestedSpace;
+  if (spaceLeft >= nodeSize + 8)
+  {
+    node_t *newNode = (node_t *)((char *)allocated + requestedSpace + nodeSize);
+    newNode->size = spaceLeft - nodeSize;
+    newNode->next = allocated->next;
+    if (allocated == g_head)
+    {
+      if (g_allocationAlgo == NEXT_FIT && g_nextFitStart == g_head)
+      {
+        g_nextFitStart = newNode;
+      }
+      g_head = newNode;
+    }
+    else
+    {
+      skipNode(allocated, newNode);
+    }
+  }
+  else
+  {
+    if (allocated == g_head)
+    {
+      if (g_allocationAlgo == NEXT_FIT && g_nextFitStart == g_head)
+      {
+        g_nextFitStart = NULL;
+      }
+      g_head = NULL;
+    }
+    else
+    {
+      skipNode(allocated, allocated->next);
+    }
+  }
+  allocated->size = requestedSpace;
+  allocated->next = MARKER;
+}
+
 void *mallocBestFit(size_t size)
 {
   node_t *curBestFit = NULL;
@@ -73,11 +130,7 @@ void *mallocBestFit(size_t size)
   {
     if (ptr->size >= adjustedSize)
     {
-      if (curBestFit == NULL)
-      {
-        curBestFit = ptr;
-      }
-      else if (curBestFit->size > ptr->size)
+      if (curBestFit == NULL || curBestFit->size > ptr->size)
       {
         curBestFit = ptr;
       }
@@ -88,55 +141,112 @@ void *mallocBestFit(size_t size)
   {
     return NULL;
   }
-  size_t spaceLeft = curBestFit->size - adjustedSize;
-  if (spaceLeft >= nodeSize + 8)
-  {
-    node_t *newNode = (node_t *)((char *)curBestFit + adjustedSize + nodeSize);
-    newNode->size = spaceLeft - nodeSize;
-    newNode->next = curBestFit->next;
-    // TODO: Check if we are allowed to use 2 pointers in our node structure.
-    if (curBestFit == g_head)
-    {
-      g_head = newNode;
-    }
-    else
-    {
-      for (node_t *p = g_head; p != NULL; p = p->next)
-      {
-        if (p->next == curBestFit)
-        {
-          p->next = newNode;
-          break;
-        }
-      }
-    }
-  }
-  else
-  {
-    if (curBestFit == g_head)
-    {
-      g_head = NULL;
-    }
-    else
-    {
-      for (node_t *p = g_head; p != NULL; p = p->next)
-      {
-        if (p->next == curBestFit)
-        {
-          p->next = curBestFit->next;
-          break;
-        }
-      }
-    }
-  }
-  curBestFit->size = adjustedSize;
-  curBestFit->next = MARKER;
+  umallocSpaceSaver(curBestFit, adjustedSize);
+
   return (void *)((char *)curBestFit + nodeSize);
+}
+
+void *mallocWorstFit(size_t size)
+{
+  node_t *curWorstFit = NULL;
+  node_t *ptr = g_head;
+  int adjustedSize = roundup(size);
+
+  while (ptr != NULL)
+  {
+    if (ptr->size >= adjustedSize)
+    {
+      if (curWorstFit == NULL || curWorstFit->size < ptr->size)
+      {
+        curWorstFit = ptr;
+      }
+    }
+    ptr = ptr->next;
+  }
+  if (curWorstFit == NULL)
+  {
+    return NULL;
+  }
+  umallocSpaceSaver(curWorstFit, adjustedSize);
+
+  return (void *)((char *)curWorstFit + nodeSize);
+}
+
+void *mallocFirstFit(size_t size)
+{
+  node_t *ptr = g_head;
+  node_t *firstFit = NULL;
+  int adjustedSize = roundup(size);
+
+  while (ptr != NULL)
+  {
+    if (ptr->size >= adjustedSize)
+    {
+      firstFit = ptr;
+      break;
+    }
+    ptr = ptr->next;
+  }
+  if (firstFit == NULL)
+  {
+    return NULL;
+  }
+  umallocSpaceSaver(firstFit, adjustedSize);
+
+  return (void *)((char *)firstFit + nodeSize);
+}
+
+void *mallocNextFit(size_t size)
+{
+  int adjustedSize = roundup(size);
+  node_t *nextFit = NULL;
+  node_t *ptr = g_nextFitStart;
+
+  while (ptr != NULL)
+  {
+    if (ptr->size >= adjustedSize)
+    {
+      nextFit = ptr;
+      g_nextFitStart = nextFit->next;
+      if (g_nextFitStart == NULL)
+      {
+        g_nextFitStart = g_head;
+      }
+      break;
+    }
+    ptr = ptr->next;
+  }
+  if (ptr == NULL)
+  {
+    ptr = g_head;
+    while (ptr != g_nextFitStart)
+    {
+      if (ptr->size >= adjustedSize)
+      {
+        nextFit = ptr;
+        g_nextFitStart = nextFit->next;
+        if (g_nextFitStart == NULL)
+        {
+          g_nextFitStart = g_head;
+        }
+        break;
+      }
+      ptr = ptr->next;
+    }
+  }
+  if (nextFit == NULL)
+  {
+    return NULL;
+  }
+
+  umallocSpaceSaver(nextFit, adjustedSize);
+  // printf("g_nextFitStart: %p, head: %p\n, ", g_nextFitStart, g_head);
+  return (void*)((char*)nextFit + nodeSize);
 }
 
 void *umalloc(size_t size)
 {
-  if (g_head == NULL) 
+  if (g_head == NULL)
   {
     return NULL;
   }
@@ -144,6 +254,13 @@ void *umalloc(size_t size)
   {
   case BEST_FIT:
     return mallocBestFit(size);
+  case WORST_FIT:
+    return mallocWorstFit(size);
+  case FIRST_FIT:
+    return mallocFirstFit(size);
+  case NEXT_FIT:
+    return mallocNextFit(size);
+
   default:
     printf("Not implemented.");
   }
@@ -152,38 +269,57 @@ void *umalloc(size_t size)
 
 void coalesce()
 {
-  node_t *ptr = g_head;
-  char *end = NULL;
-  while (ptr != NULL)
+  int hasCoalesced = 0;
+  do
   {
-    end = (char *)ptr + nodeSize + ptr->size;
-    // printf("Head: %p\n", g_head);
-    node_t *ptrEnd = (node_t *)(end);
-    node_t *cmp = ptr->next;
-    while (cmp != NULL)
+    // printf("coalesce g_nextFitStart: %p, head: %p\n", g_nextFitStart, g_head);
+    node_t *ptr = g_head;
+    char *end = NULL;
+    hasCoalesced = 0;
+    while (ptr != NULL)
     {
-      // printf("ptr: %p, cmp: %p, ptrEnd: %p\n", ptr, cmp, ptrEnd);
-      if (cmp == ptrEnd)
+      end = (char *)ptr + nodeSize + ptr->size;
+      // printf("Head: %p\n", g_head);
+      node_t *ptrEnd = (node_t *)(end);
+      node_t *cmp = g_head;
+      if (g_head == ptrEnd)
       {
-        // printf("Entered Coalesce if\n");
         ptr->size = ptr->size + ptrEnd->size + nodeSize;
-        ptr->next = ptrEnd->next;
+        g_head = g_head->next;
+        if (g_allocationAlgo == NEXT_FIT && g_nextFitStart == g_head)
+        {
+          g_nextFitStart = g_head;
+        }
+        hasCoalesced = 1;
       }
-      cmp = cmp->next;
+      while (cmp != NULL && cmp->next != NULL)
+      {
+        // printf("ptr: %p, cmp: %p, cmp next: %p, ptrEnd: %p\n", ptr, cmp, cmp->next, ptrEnd);
+        if (cmp->next == ptrEnd)
+        {
+          ptr->size = ptr->size + ptrEnd->size + nodeSize;
+          cmp->next = ptrEnd->next;
+          hasCoalesced = 1;
+          // printf("Coalescing node: %p with node %p, new size of node: %u\n", ptr, ptrEnd, ptr->size);
+        }
+        cmp = cmp->next;
+      }
+      ptr = ptr->next;
     }
-    ptr = ptr->next;
-  }
+  } while (hasCoalesced);
 }
 
 int ufree(void *ptr)
 {
   node_t *nodePtr = (node_t *)((char *)ptr - nodeSize);
+  // printf("ufree ptr: %p, ptr next: %p\n", nodePtr, nodePtr->next);
   if (nodePtr->next != MARKER)
   {
     return -1;
   }
-  nodePtr->next = g_head;
+  node_t *tmp = g_head;
   g_head = nodePtr;
+  nodePtr->next = tmp;
   coalesce();
   return 0;
 }
--- csus/csc139_posnett/Linux_Memory_Allocation/umem_test.c
+++ csus/csc139_posnett/Linux_Memory_Allocation/umem_test.c
@@ -3,9 +3,9 @@
 
 #include "umem.h"
 
-int test_umeminit(size_t size)
+int test_umeminit(size_t size, int allocationAlgo)
 {
-  int result = umeminit(size, BEST_FIT);
+  int result = umeminit(size, allocationAlgo);
   if (result == 0)
   {
     printf("Passed\n");
@@ -37,22 +37,28 @@ int test_ufree(void *ptr)
   {
     printf("Passed\n");
   }
-  return 0;
+  return result;
 }
 
 int main()
 {
-  test_umeminit(128);
+  test_umeminit(128, NEXT_FIT);
   test_umemdump();
   // Scenario 1: Allocate block and immediately deallocate it.
   // Look for coalesce to return the initial memory.
+  printf("Scenario 1: \n");
   void *result1 = test_umalloc(75);
   test_umemdump();
-  test_ufree(result1);
+  int freeTest = test_ufree(result1);
+  if (freeTest == -1)
+  {
+    printf("Failed\n");
+  }
   test_umemdump();
   
   // Scenario 2: Allocate 3 blocks and deallocate 2 of them.
   // Observe memory fragmentation with no coalescing.
+  printf("Scenario 2: \n");
   void *result2 = test_umalloc(15);
   test_umemdump();
   void *result3 = test_umalloc(16);
@@ -65,6 +71,21 @@ int main()
   test_ufree(result4);
   test_umemdump();
   test_ufree(result2);
+  test_umemdump();
+
+  // Scenario 3: Create 1 small block and 1 large block of free memory,
+  // And then test policies when allocating.
+  printf("Scenario 3: \n");
+  void *result5 = test_umalloc(31);
+  void *result6 = test_umalloc(20);
+  test_umemdump();
+  test_ufree(result5);
+  test_umemdump();
+  void *result7 = test_umalloc(8);
+  test_umemdump();
+  test_ufree(result6);
+  test_umemdump();
+  test_ufree(result7);
   test_umemdump();
 
   return 0;
